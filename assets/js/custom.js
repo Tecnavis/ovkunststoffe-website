ad52cb6adc5a89164a0999ccb9cf22e994c08984
thabumon <52034689+thabumon@users.noreply.github.com>
2024-03-22 16:42:46 +0530

    edtz

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -187,10 +187,10 @@
 
 		$("#preloader").animate({
 			'opacity': '0'
-		}, 600, function(){
+		}, 300, function(){
 			setTimeout(function(){
 				$("#preloader").css("visibility", "hidden").fadeOut();
-			}, 300);
+			}, 200);
 		});
 	});
 
